4a42ecd177440f2e660a17dcf24b3e443a299fe1
Joe Lamb <joe@elevenplustwo.co.uk>
2018-09-19 14:18:25 +0100

    longest string test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,13 @@
 const functions = require('../src/index');
 
-test('Addition', function(){
+test('Addition', function() {
   const expected = 5;
   const result = functions.add(2, 3);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
+});
+
+test('Longest String', function() {
+  const expected = 'abcd';
+  const result = functions.longestString(['a', 'abc', 'abcd', 'ab']);
+  expect(result).toEqual(expected);
 });
