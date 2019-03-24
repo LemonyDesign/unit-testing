8a5312b753f7aa1bc6d0c6cfc21863c6700ba983
Lemony <lemonystudio@gmail.com>
2019-03-24 11:08:03 +0000

    Unit test coverage completed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ function add(a, b) {
   return a + b;
 }
 
-exports.add = add;
-
 function longestString(array) {
-  let longest = '';
+  let longest = "";
   array.forEach(item => {
     if (item.length > longest.length) {
       longest = item;
@@ -13,8 +11,6 @@ function longestString(array) {
   });
   return longest;
 }
-
-exports.longestString = longestString;
 
 const l337 = string => {
   swapRules = {
@@ -33,18 +29,143 @@ const l337 = string => {
   };
 
   return string
-    .split('')
+    .split("")
     .map(item => {
       return swapRules.hasOwnProperty(item) ? (item = swapRules[item]) : item;
     })
-    .join('');
+    .join("");
 };
 
-exports.l337 = l337;
+// if the i counter is greater than the index of the array (first instance of that item in the array) - it's a duplicate; so filter these out
+const uniqueStrings = array => {
+  return array.filter((item, i) => {
+    return !(i > array.indexOf(item));
+  });
+};
+
+// Here is the constructor, test with a new instance
+function Developer(name, languages) {
+  this.name = name;
+  this.languages = languages;
+}
+
+Developer.prototype.learnLanguage = function(language) {
+  this.languages.includes(language)
+    ? this.languages
+    : this.languages.push(language);
+};
+
+const stringsConcat = array =>
+  array.filter(item => (typeof item === "string" ? item : false)).join(" ");
+
+const negativeOnly = array =>
+  array.filter(number => (number < 0 ? number : false));
+
+const camelise = string => {
+  const arr = string.split(" ");
+
+  const capsArr = arr.map(word => {
+    firstLetter = word[0].toUpperCase();
+    rem = word.slice(1);
+    return (word = firstLetter.concat(rem));
+  });
+
+  return arr[0].concat(capsArr.slice(1).join(""));
+};
+
+const isPrime = number => {
+  let arr = [];
+  for (let i = 2; i < number; i++) {
+    arr.push(i);
+  }
+  const notPrime = arr.filter(item => number % item === 0);
+  return notPrime.length === 0 ? true : false;
+};
+
+function Walker(journey) {
+  this.journey = [[0, 0]];
+}
+
+Walker.prototype.walk = function(direction, number) {
+  array = this.journey;
+
+  let last = array[array.length - 1];
+
+  if (direction === "N") {
+    this.journey.push([last[0], last[1] + number]);
+  }
+  if (direction === "S") {
+    this.journey.push([last[0], last[1] - number]);
+  }
+  if (direction === "W") {
+    this.journey.push([last[0] - number, last[1]]);
+  }
+  if (direction === "E") {
+    this.journey.push([last[0] + number, last[1]]);
+  }
+
+  return this;
+};
+
+Walker.prototype.currentLocation = function() {
+  array = this.journey;
+  return array[array.length - 1];
+};
+
+Walker.prototype.pathTaken = function() {
+  array = this.journey;
+  const itemOutput = array.map(item => {
+    const joinItem = item.join(", ");
+    return `<li>${joinItem}</li>`;
+  });
+
+  const list = itemOutput.join("");
+
+  return `<ul>${list}</ul>`;
+};
+
+Walker.prototype.totalSteps = function() {
+  const array = this.journey;
+  const arrOne = array.reduce((acc, item, i, arr) => {
+    // set prev for first in array AS first in array - or it steps back to undefined
+    let prev0 = arr[0][0];
+    let prev1 = arr[0][1];
+
+    if (i > 1) {
+      prev0 = arr[i - 1][0];
+      prev1 = arr[i - 1][1];
+    }
+
+    acc.push(Math.abs(item[0] - prev0), Math.abs(item[1] - prev1));
+    return acc;
+  }, []);
+
+  return arrOne.reduce((acc, item) => acc + item, 0);
+};
+
+module.exports = {
+  add,
+  longestString,
+  l337,
+  uniqueStrings,
+  Developer,
+  stringsConcat,
+  negativeOnly,
+  camelise,
+  isPrime,
+  Walker
+};
+
+/*
+ *
+ * ALTERNATIVES to uniqueStrings
+ *
+ */
 
 // is it there?
 // if it is there - return value
 // if it not there yet - create property false value, and test it equals false - which returns true - so it keeps in the array when filtering
+
 // const uniqueStrings = array => {
 //   const lookUp = {};
 //   return array.filter(item => {
@@ -58,12 +179,3 @@ exports.l337 = l337;
 //     return item in unique ? unique[item] : (unique[item] = false) === false;
 //   });
 // };
-
-// if the i counter is greater than the index of the array (first instance of that item in the array) - it's a duplicate; so filter these out
-const uniqueStrings = array => {
-  return array.filter((item, i) => {
-    return !(i > array.indexOf(item));
-  });
-};
-
-exports.uniqueStrings = uniqueStrings;
--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,198 @@
-const functions = require('../src/index');
+const functions = require("../src/index");
 
-test('Addition', function() {
+test("1. Addition", function() {
   const expected = 5;
   const result = functions.add(2, 3);
+  expect(result).toBe(expected);
+});
+
+test("2. Longest String", function() {
+  const expected = "abcd";
+  const result = functions.longestString(["a", "abc", "abcd", "ab", "defg"]);
+  expect(result).toBe(expected);
+});
+
+test("3. L337", () => {
+  const expected = "112345677890";
+  const result = functions.l337("ilzeasgtybqo");
+  expect(result).toBe(expected);
+});
+
+test("4. Unique strings", () => {
+  const expected = ["a", "ab", "c"];
+  const result = functions.uniqueStrings(["a", "ab", "ab", "c", "a"]);
   expect(result).toEqual(expected);
 });
 
-test('Longest String', function() {
-  const expected = 'abcd';
-  const result = functions.longestString(['a', 'abc', 'abcd', 'ab', 'defg']);
+test("5. Developer", () => {
+  const expected = {
+    name: "David",
+    languages: ["JavaScript", "Ruby", "TypeScript", "C++"]
+  };
+  // new instance for testing
+  const result = new functions.Developer("David", [
+    "JavaScript",
+    "Ruby",
+    "TypeScript",
+    "C++"
+  ]);
+
   expect(result).toEqual(expected);
 });
 
-test('L337', () => {
-  const expected = '112345677890';
-  const result = functions.l337('ilzeasgtybqo');
+test("6. Learn - add new language", () => {
+  const expected = {
+    name: "David",
+    languages: ["JavaScript", "Ruby", "TypeScript", "C++", "PHP"]
+  };
+
+  // new instance for testing
+  const dev = new functions.Developer("David", [
+    "JavaScript",
+    "Ruby",
+    "TypeScript",
+    "C++"
+  ]);
+
+  dev.learnLanguage("PHP");
+
+  const result = dev;
+
   expect(result).toEqual(expected);
 });
 
-test('Unique strings', () => {
-  const result = functions.uniqueStrings(['a', 'ab', 'ab', 'c', 'a']);
-  const expected = ['a', 'ab', 'c'];
+test("7. Learn - is unique", () => {
+  const expected = {
+    name: "David",
+    languages: ["JavaScript", "Ruby", "TypeScript", "C++"]
+  };
+
+  // new instance for testing
+
+  const dev = new functions.Developer("David", [
+    "JavaScript",
+    "Ruby",
+    "TypeScript",
+    "C++"
+  ]);
+
+  dev.learnLanguage("Ruby");
+
+  const result = dev;
+
+  expect(result).toEqual(expected);
+});
+
+test("8. Strings concatenated", () => {
+  const expected = "This is the resultant string";
+  const result = functions.stringsConcat([
+    "This",
+    10,
+    11,
+    "is",
+    "the",
+    6,
+    "resultant",
+    "string"
+  ]);
+
+  expect(result).toEqual(expected);
+});
+
+test("9. Negative numbers only", () => {
+  const expected = [-2, -10, -10000, -2.5];
+  const result = functions.negativeOnly([
+    -2,
+    2,
+    -10,
+    10,
+    -10000,
+    10000,
+    -2.5,
+    2.5
+  ]);
+  expect(result).toEqual(expected);
+});
+
+test("10. Camelise", () => {
+  const expected = "camelCaseWord";
+  const result = functions.camelise("camel case word");
+  expect(result).toBe(expected);
+});
+
+test("11. Is prime", () => {
+  const expected = true;
+  const result = functions.isPrime(13);
+  expect(result).toBe(expected);
+});
+
+test("12. Is not prime", () => {
+  const expected = false;
+  const result = functions.isPrime(15);
+  expect(result).toBe(expected);
+});
+
+test("13. Walkabout", () => {
+  const expected = {
+    journey: [[0, 0]]
+  };
+  // new instance to test
+  const result = new functions.Walker();
+  expect(result).toEqual(expected);
+});
+
+test("14. Walk", () => {
+  const expected = {
+    journey: [[0, 0], [2, 0], [2, 3]]
+  };
+  // new instance to test
+  const myJourney = new functions.Walker();
+
+  const result = myJourney.walk("E", 2);
+  myJourney.walk("N", 3);
+
+  expect(result).toEqual(expected);
+});
+
+test("15. Current location", () => {
+  const expected = [10, 5];
+
+  // new instance to test
+  const myJourney = new functions.Walker();
+
+  myJourney.walk("E", 10);
+  myJourney.walk("N", 5);
+
+  const result = myJourney.currentLocation();
+
+  expect(result).toEqual(expected);
+});
+
+test("16. List coordinates", () => {
+  const expected = "<ul><li>0, 0</li><li>10, 0</li><li>10, 5</li></ul>";
+
+  // new instance to test
+  const myJourney = new functions.Walker();
+
+  myJourney.walk("E", 10);
+  myJourney.walk("N", 5);
+
+  const result = myJourney.pathTaken();
+
+  expect(result).toEqual(expected);
+});
+
+test("17. Total steps", () => {
+  const expected = 10;
+
+  // new instance to test
+  const myJourney = new functions.Walker();
+
+  myJourney.walk("E", 2);
+  myJourney.walk("N", 3);
+  myJourney.walk("S", 2);
+  myJourney.walk("W", 3);
+
+  const result = myJourney.totalSteps();
   expect(result).toEqual(expected);
 });
